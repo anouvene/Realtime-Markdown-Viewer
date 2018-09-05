2a23a5cd1d40f1e8edbf656ce38de8da531a98b6
anouvene <nouvene@gmail.com>
2018-09-05 10:22:26 +0200

    Add link

--- converter/markdown.js
+++ converter/markdown.js
@@ -17,13 +17,24 @@
   return str;
  }
 
-var markdown = {
-  parse: function (str, strict) {
-    'use strict';
-    str = parseHeadline(str);
-    str = parseHorizontaleLine(str);
-    return str;
+ var parseLink = function(str) {
+  var linkRegExp = /\[([^\[]+)\]\(([^\)]+)\)/;
+  var stra = [];
+  while ((stra = linkRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '<a ' + 'href="' + stra[2] + '">' + stra[1] + '</a>');
   }
-};
+  return str;
+ }
 
-module.exports = markdown;
+
+ var markdown = {
+   parse: function (str, strict) {
+     'use strict';
+     str = parseHeadline(str);
+     str = parseHorizontaleLine(str);
+     str = parseLink(str);
+     return str;
+   }
+ };
+
+ module.exports = markdown;
