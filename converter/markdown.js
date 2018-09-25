08daaef1d6f05957d165e4b558ac597e2bd9782f
anouvene <nouvene@gmail.com>
2018-09-25 05:54:56 +0200

    add code in parser

--- converter/markdown.js
+++ converter/markdown.js
@@ -54,8 +54,16 @@ var parseStrong = function(str) {
   return str;
  }
 
+ var parseCode = function(str) {
+  var codeRegExp = /`{1}(\w+)`{1}/;
+  var stra = [];
+  while ((stra = codeRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '<pre>' + stra[1] + '</pre>');
+  }
+  return str;
+ }
 
-var markdown = {
+ var markdown = {
   parse: function (str, strict) {
     'use strict';
     str = parseHeadline(str);
@@ -64,8 +72,9 @@ var markdown = {
     str = parseStrong(str);
     str = parseHorizontaleLine(str);
     str = parseLink(str);
+    str = parseCode(str);
     return str;
   }
-};
+ };
 
 module.exports = markdown;
